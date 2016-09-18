d2afcd9940564ddd0674097fa2312bfdb8328106
MechMel <MelchiahMauck@gmail.com>
2016-09-17 21:57:34 -0700

    broke other stuff

---
+++ Assets/scripts/EditorTest.cs
@@ -0,0 +1,34 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+
+[CustomEditor(typeof(MasterControlScripTest))]
+public class EditorTest : Editor
+{
+    public override void OnInspectorGUI()
+    {
+
+        EditorGUI.BeginChangeCheck();
+
+        MasterControlScripTest myTarget = (MasterControlScripTest)target;
+
+        myTarget.experience = EditorGUILayout.IntField("Experience", myTarget.experience);
+        EditorGUILayout.LabelField("Trial", myTarget.trial.ToString());
+        EditorGUILayout.LabelField("Level", myTarget.Level.ToString());
+        myTarget.SetLevel();
+
+        // Block of code with controls
+        // that may set GUI.changed to true.
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            // Code to execute if GUI.changed
+            // was set to true inside the block of code above.
+            Debug.Log("Value was changed do stuff here");
+        }
+    }
+
+
+}
---
+++ Assets/scripts/MasterControlScripTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MasterControlScripTest : MonoBehaviour
+{
+    public int experience;
+
+    public int trial;
+
+    public int Level
+    {
+        get { return experience / 750; }
+    }
+
+    public void SetLevel()
+    {
+        trial = experience;
+    }
+}
