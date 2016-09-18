d68e271f30352e46483d2e1d11005e06bced2a98
MechMel <MelchiahMauck@gmail.com>
2016-09-18 09:57:13 -0700

    Commented TIKLevelControlEditor

--- Assets/scripts/Editor/TIKLevelControlEditor.cs
+++ Assets/scripts/Editor/TIKLevelControlEditor.cs
@@ -6,27 +6,27 @@ using UnityEditor;
 [CustomEditor(typeof(TIKLevelControl))]
 public class TIKEditorLevelControl : Editor
 {
-    //
+    // This will overide unity's standard GUI
     public override void OnInspectorGUI()
     {
-        //
+        // Define the target script to use
         TIKLevelControl myTarget = (TIKLevelControl)target;
-        //
+        // Create a slot to put the TextAsset for this level's map in
         myTarget.tileMapTextAsset = (TextAsset)EditorGUILayout.ObjectField("Tile Map", myTarget.tileMapTextAsset, typeof(TextAsset), false);
-        //
+        // Check to see if the GUI has been changed
         EditorGUI.BeginChangeCheck();
+        // When the GUI is changed
         if (myTarget.UpdateGUI())
         {
-            //
+            // Create a slot to put the Texture2D from each Tileset in
             for (int currentTileset = 0; currentTileset < myTarget.tilesetTextures.Length; currentTileset++)
             {
-                //
                 myTarget.tilesetTextures[currentTileset] =
                     (Texture2D)EditorGUILayout.ObjectField
                     (myTarget.levelMap.tilesets[currentTileset].name, myTarget.tilesetTextures[currentTileset], typeof(Texture2D), false);
             }
         }
-        //
+        // Stop checking to see if the GUI has been changed
         EditorGUI.EndChangeCheck();
     }
 }
