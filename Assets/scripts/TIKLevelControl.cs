1d06a59577e03e5cc9edea6f05aa7906cf1d6553
MechMel <MelchiahMauck@gmail.com>
2016-09-18 22:41:27 -0700

    TIKLevelControl Now Remembers Textures the User put in for each Tileset

--- Assets/scripts/TIKLevelControl.cs
+++ Assets/scripts/TIKLevelControl.cs
@@ -4,16 +4,26 @@ using System.Collections.Generic;
 
 public class TIKLevelControl : MonoBehaviour
 {
+    #region Data for this Level
     // This is the text asset for this level's map
     public TextAsset tileMapTextAsset;
-    // This is a copy of the tileMapTextAsset. It is used to determine if tileMapText asset has ben changed
-    private TextAsset oldTileMapTextAsset;
-    // This is an array of textures for each tileset
-    public Texture2D[] tilesetTextures;
-    // Reference to TIK's jsonUtilies class
-    private TIKJsonUtilities jsonUtilities = new TIKJsonUtilities();
     // This is the TIKMap for this level
     public TIKMap levelMap;
+    // This is an array of textures for each tileset
+    public Texture2D[] tilesetTextures;
+    #endregion
+
+    #region Tiled Map Vhange Variables
+    // This is a copy of the tileMapTextAsset. It is used to determine if tileMapText asset has ben changed
+    private TextAsset oldTileMapTextAsset;
+    // This is a copy of levelMap. It is used to see what has changed between this map and the new one
+    public TIKMap oldLevelMap;
+    // This is an array of textures for each tileset in the new TextAsset for this level's map
+    public Texture2D[] newTilesetTextures;
+    #endregion
+
+    // Reference to TIK's jsonUtilies class
+    private TIKJsonUtilities jsonUtilities = new TIKJsonUtilities();
 
 
     // When TIKLevelControlEditor calls this function variables are checked and updated
@@ -38,14 +48,36 @@ public class TIKLevelControl : MonoBehaviour
     }
 
 
-    //
+    // When this is called it updates the nessecary variable for when tileMapTextAsset has been changed
     private void tileMapTextAssetChanged()
     {
-        // Update oldTileMapTextAsset for use for next time the GUI is changed
-        oldTileMapTextAsset = tileMapTextAsset;
+        // Remeber the last levelMap
+        oldLevelMap = levelMap;
         // Create a new TIKMap based on the new textAsset file
         levelMap = jsonUtilities.CreateTIKMapFromTextAsset(tileMapTextAsset);
-        // Make tilesetTextures have enough slots for each tilest in this levelMap
-        tilesetTextures = new Texture2D[levelMap.tilesets.Length];
+        // Make newTilesetTextures have enough slots for each tilest in the new map
+        newTilesetTextures = new Texture2D[levelMap.tilesets.Length];
+        // If tilesetTextures has been instatiated
+        if (tilesetTextures != null)
+        {
+            // For each tileset in the new map
+            for (int newTileset = 0; newTileset < newTilesetTextures.Length; newTileset++)
+            {
+                // Go through each tileset in the old map
+                for (int oldTileset = 0; oldTileset < tilesetTextures.Length; oldTileset++)
+                {
+                    // If these tilesets have the same name
+                    if (levelMap.tilesets[newTileset].name == oldLevelMap.tilesets[oldTileset].name)
+                    {
+                        // Then remember the texture that the user put in
+                        newTilesetTextures[newTileset] = tilesetTextures[oldTileset];
+                    }
+                }
+            }
+        }
+        // Update oldTileMapTextAsset for use for next time the GUI is changed
+        oldTileMapTextAsset = tileMapTextAsset;
+        // Update tilesetTextures to have the new tilesets
+        tilesetTextures = newTilesetTextures;
     }
 }
