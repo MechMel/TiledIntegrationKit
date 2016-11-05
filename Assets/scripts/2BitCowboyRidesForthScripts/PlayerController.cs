bc538a210e9d3a8c8245fcb8b398514a255fb9df
Mysnomer0 <milan.mauck@pcc.edu>
2016-11-05 14:06:00 -0700

    Added 2BitCowboyRidesForthScripts folder
    Made a test scene, and started on the PlayerController class

---
+++ Assets/scripts/2BitCowboyRidesForthScripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    // The PlayerController class is the basic controller for the player, and the base class for add-ons
+
+    // The player speed
+    [HideInInspector]
+    public float playerSpeed = 1f;
+    // The player jump speed
+    [HideInInspector]
+    public float playerJumpSpeed = 8f;
+    // The player animation speed
+    // NOT YET IMPLEMENTED
+    [HideInInspector]
+    public float playerAnimationSpeed = 8f;
+
+    void Start ()
+    {
+	    
+	}
+	
+	void Update ()
+    {
+	
+	}
+}
