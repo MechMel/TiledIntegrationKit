878ac98de8fa0afa5793b1cc72e7d18957615ea4
Mysnomer0 <milan.mauck@pcc.edu>
2016-12-24 10:47:27 -0800

    Added All Pickups/Buffs/ItemBox

--- Assets/scripts/2BitCowboyRidesForthScripts/StaticObjects/PickupBehaviour.cs
+++ Assets/scripts/2BitCowboyRidesForthScripts/StaticObjects/PickupBehaviour.cs
@@ -13,16 +13,21 @@ public class PickupBehaviour : MonoBehaviour {
     public PickupType pickupType;
 
     #region Basic Stats
+    [Header("---ITEMBOX VARIABLES---")]
     // The health of the pickup
     public int health;
     // Whether the pickup is affected by gravity
     public bool isAffectedByGravity;
+    // The array of images
+    [Tooltip("The array of images for breakable pickups. Only set for ITEMBOX pickups.")]
+    public Sprite[] imagesOfPickup;
     // The array of what is possible to drop
-    [Tooltip("The possible drops of the object. Drops a random object out of the array.")]
+    [Tooltip("Drops a random object out of the array. Only set for ITEMBOX pickups.")]
     public GameObject[] possibleDrops;
     // The amount of objects to drop
     [Tooltip("The amount of items to drop.")]
     public int amountToDrop;
+    
     #endregion
     #region Components
     // The sprite renderer of the pickup
@@ -34,25 +39,59 @@ public class PickupBehaviour : MonoBehaviour {
     {
         // Setup the sprite renderer
         sprRend = GetComponent<SpriteRenderer>();
-	}
-	
-	
+
+        // Check to turn off gravity
+        if (isAffectedByGravity)
+            GetComponent<Rigidbody2D>().isKinematic = false;
+        else
+            GetComponent<Rigidbody2D>().isKinematic = true;
+
+        // If pickup is a BUFF
+        if (pickupType == PickupType.BUFF)
+            // Set the BoxCollider2D to a trigger
+            GetComponent<BoxCollider2D>().isTrigger = true;
+    }
+		
 	void Update ()
     {
-	    // Check if destroyed
-        if(health <= 0)
+        #region ITEMBOX
+        if (pickupType == PickupType.ITEMBOX)
         {
-            // Destroy the object
-            Destroy(gameObject);
-            // Drop the right amount of items based on the given stats
-            for(int i = 0; i < amountToDrop; i++)
+            // Check if destroyed
+            if (health <= 0)
             {
-                // Create a random pickup from the possibleDrops array
-                Instantiate(possibleDrops[Mathf.RoundToInt(Random.Range(0, possibleDrops.Length))], 
-                    new Vector2(transform.position.x - i / 2, transform.position.y), Quaternion.identity);
+                // Destroy the object
+                Destroy(gameObject);
+                // Drop the right amount of items based on the given stats
+                for (int i = 0; i < amountToDrop; i++)
+                {
+                    // Create a random pickup from the possibleDrops array
+                    Instantiate(possibleDrops[Mathf.RoundToInt(Random.Range(0, possibleDrops.Length))],
+                        new Vector2(transform.position.x - i / 2, transform.position.y), Quaternion.identity);
+                }
+            }
+            else
+            {
+                // Update the sprite of the pickup based on the health
+                sprRend.sprite = imagesOfPickup[Mathf.RoundToInt(health - 1)];
             }
         }
+        #endregion
+    }
 
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // If the pickup is of BUFF pickup type
+        if (pickupType == PickupType.BUFF)
+        {
+            // Destroy itself
+            Destroy(gameObject);
+        }
+    }
 
-	}
+    void Hit()
+    {
+        // Decrement health when hit
+        health--;
+    }
 }
