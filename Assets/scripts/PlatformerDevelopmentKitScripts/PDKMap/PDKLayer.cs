4cb4d4df048736e4819fb76fffc43be4fb783909
MechMel <MelchiahMauck@gmail.com>
2016-12-23 23:20:51 -0800

    Added GetObjectsInRect to PDKLayer
    Note: this is untested.

--- Assets/scripts/PlatformerDevelopmentKitScripts/PDKMap/PDKLayer.cs
+++ Assets/scripts/PlatformerDevelopmentKitScripts/PDKMap/PDKLayer.cs
@@ -39,4 +39,27 @@ public class PDKLayer
             }
         }
     }
+    
+
+    // Returns a refrence to all objects in a rect of the object map
+    public HashSet<PDKObject> GetObjectsInRect(Rect rectToGet)
+    {
+        // Create a new hash set to put all objects in the given rect into
+        HashSet<PDKObject> objectsInRect = new HashSet<PDKObject>();
+
+        // Go through each collumn in the given rect
+        for (int currentCollumnIndex = (int)rectToGet.xMin; currentCollumnIndex < (int)rectToGet.xMax; currentCollumnIndex++)
+        {
+            // Go through each row in this collumn
+            for (int currentRowIndex = (int)rectToGet.yMin; currentRowIndex < (int)rectToGet.yMax; currentRowIndex++)
+            {
+                // Add each object from this slot in the object map
+                foreach (PDKObject currentObject in objectMap[currentCollumnIndex][currentRowIndex])
+                {
+                    objectsInRect.Add(currentObject);
+                }
+            }
+        }
+        return objectsInRect;
+    }
 }
