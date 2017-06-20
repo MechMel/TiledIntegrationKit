ba66b36a7e5ffd966fafa585e14351eda0540359
MechMel <MelchiahMauck@gmail.com>
2017-06-20 13:15:10 -0700

    Updated Player Pick Up

--- Assets/Resources/prefabs/Pickups/Bounty/Bounty.cs
+++ Assets/Resources/prefabs/Pickups/Bounty/Bounty.cs
@@ -2,6 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Bounty stats
+public enum ObjectTypes
+{
+    CRATE,
+    COIN_LARGE,
+    BARREL,
+    DOOR,
+    COIN,
+    BOTTLE,
+    CRATE_COIN,
+    CRATE_SCORPIAN,
+    CRATE_HEALTH,
+    CRATE_EXPLOSION,
+    HEALTH,
+    SHEEP,
+    CHICKEN,
+    SNAKE,
+    RATTLE_SNAKE,
+    SPIDER,
+    TARANTULA,
+    BAT,
+    SCORPION,
+    CRAWDAD,
+    LIZARD,
+    VULTURE,
+    PIRANHA,
+    RAT,
+    ARMADILLO,
+    BANDIT
+}
 
 public struct Bounty
 {
