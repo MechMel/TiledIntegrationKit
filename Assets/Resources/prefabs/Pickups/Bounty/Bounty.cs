154ad43e353477c34e4a0718500cd36f5c6aa824
MechMel <MelchiahMauck@gmail.com>
2017-06-20 13:08:10 -0700

    Started Fixing the Bounty

---
+++ Assets/Resources/prefabs/Pickups/Bounty/Bounty.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public struct Bounty
+{
+    public int numberRequirement;
+    public int reward;
+    public ObjectTypes objectType;
+}
--- Assets/playerPickUp.cs
+++ Assets/playerPickUp.cs
@@ -2,75 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerPickUp : MonoBehaviour {
+// Bounty stats
+public enum ObjectTypes
+{
+    CRATE,
+    COIN_LARGE,
+    BARREL,
+    DOOR,
+    COIN,
+    BOTTLE,
+    CRATE_COIN,
+    CRATE_SCORPIAN,
+    CRATE_HEALTH,
+    CRATE_EXPLOSION,
+    HEALTH,
+    SHEEP,
+    CHICKEN,
+    SNAKE,
+    RATTLE_SNAKE,
+    SPIDER,
+    TARANTULA,
+    BAT,
+    SCORPION,
+    CRAWDAD,
+    LIZARD,
+    VULTURE,
+    PIRANHA,
+    RAT,
+    ARMADILLO,
+    BANDIT
+}
 
-
-    public class Bounty
-    {
-
-        // Bounty stats
-        public enum ObjectTypes
-        {
-            CRATE,
-            COIN_LARGE,
-            BARREL,
-            DOOR,
-            COIN,
-            BOTTLE,
-            CRATE_COIN,
-            CRATE_SCORPIAN,
-            CRATE_HEALTH,
-            CRATE_EXPLOSION,
-            HEALTH,
-            SHEEP,
-            CHICKEN
-        }
-
-        public int numberRequirement;
-        public int reward;
-        public int objectType;
-        public Bounty()
-        {
-
-        }
-        public Bounty(int numberRequirement, int reward, int objectType)
-        {
-        
-        }
-    }
-
-    
-
+public class playerPickUp : MonoBehaviour
+{
+    public Dictionary<ObjectTypes,int> objectsDestroyed;
     public List<Bounty> bounties;
-    //public bool bounty;
-
-    //public enum ObjectTypes
-    //{
-    //    CRATE,
-    //    COIN_LARGE,
-    //    BARREL,
-    //    DOOR,
-    //    COIN,
-    //    BOTTLE,
-    //    CRATE_COIN,
-    //    CRATE_SCORPIAN,
-    //    CRATE_HEALTH,
-    //    CRATE_EXPLOSION,
-    //    HEALTH,
-    //    SHEEP,
-    //    CHICKEN
-    //}
-
-    //public Dictionary<ObjectTypes,int> objectsDestroyed;
-    //public Dictionary<ObjectTypes, int> bountifulBounties;
-
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        
-	}
 }
