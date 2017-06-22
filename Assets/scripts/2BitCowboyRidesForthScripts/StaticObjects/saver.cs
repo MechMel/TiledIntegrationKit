5051e41df0291e535c3825306b6b5001d99e6519
micchael collins <mike@ki-games>
2017-06-22 14:13:30 -0700

    A LOT OF STUFF

---
+++ Assets/scripts/2BitCowboyRidesForthScripts/StaticObjects/saver.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+static class saver {
+    static void blah(object player)
+    {
+
+
+        PlayerPrefs.SetInt("gold",);
+
+    }
+}
